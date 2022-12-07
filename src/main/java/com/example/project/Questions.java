bfacc2d8d1650757c3b02980633cae30368ae001
daniel <daniel.voica@stud.acs.upb.ro>
2022-12-07 22:44:38 +0200

    tema1 POO

--- src/main/java/com/example/project/Questions.java
+++ src/main/java/com/example/project/Questions.java
@@ -1,7 +1,7 @@
 package com.example.project;
 
 import java.io.*;
-
+//
 public class Questions {
 
     public static void verification_no_user_or_no_password(String[] args){
--- src/main/java/com/example/project/cleanfilequestions.java
+++ src/main/java/com/example/project/cleanfilequestions.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+//
 public class cleanfilequestions {
     public static void cleanquestions(){
         try (FileWriter fw = new FileWriter("C:\\lab POO\\aloooo\\tema-1-Huntzera13-master\\src\\main\\java\\com\\example\\project\\questions.txt", false);
